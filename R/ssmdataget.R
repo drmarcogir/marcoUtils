e3e4ffadc9416a41e1cce2078c2a1b188b8f2926
marcog77 <marco.girardello@gmail.com>
2018-01-06 12:38:29 +0100

    v1

---
+++ R/ssmdataget.R
@@ -0,0 +1,22 @@
+#'
+#'  Function creating plots from state-space model output
+#'  inmod@= paths to model objects
+#'
+
+ssmdataget<- function(inmod){
+  # list where to store results
+  results<-NULL
+  for (i in 1:length(inmod)){
+    # load model object
+    inmod1<-get(load(inmod[i]))
+    # if not hierarchical
+    if(class(inmod1)=="ssm"){
+      dplyr::select(inmod1[[1]]$summary,c(id,date,lon,lat,b)) %>% as.data.frame() ->tmp
+    } else {
+      # if hierarchical
+      dplyr::select(inmod1$summary,c(id,date,lon,lat,b)) %>% as.data.frame() ->tmp
+    }
+    tmp<-rbind(tmp,results)
+  }
+  return(results)
+}
