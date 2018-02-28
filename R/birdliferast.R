aeaa0c44a5c5eda8590cce9074ec370acd509800
marcog77 <marco.girardello@gmail.com>
2018-02-28 19:37:26 -0100

    v1

--- R/birdliferast.R
+++ R/birdliferast.R
@@ -1,19 +1,19 @@
 #' Create raster from Birdlife ranges (in shapefile format)
 #'
 #' @speclist = species names where these are stored
-#' @inpath = path where shapefiles are store 
+#' @inpath = path where shapefiles are store
 #' @rastg = raster grid
 #' @outpath = path of directory where to write the rasters
 #' @inproj= projection of input shapefiles
 #' @outproj= projection outputfiles
- 
+
 
 birdliferast<-function(speclist=NULL,inpath=NULL,outpath=NULL,rastg=NULL,season=NULL,inproj=NULL,outproj=NULL){
     results<-NULL
     inproj1<-CRS(inproj)
     outproj1<-CRS(outproj)
     for (i in 1:length(speclist)){
-        cat(as.character(speclist[i]))  
+        cat(as.character(speclist[i]))
         cat("\n")
         binomial<-paste(str_split_fixed(speclist[i],"_",3)[1:2],collapse="_")
         gridf<-rastertodf(rastg)
@@ -22,16 +22,16 @@ birdliferast<-function(speclist=NULL,inpath=NULL,outpath=NULL,rastg=NULL,season=
         tmpshp1<-spTransform(tmpshp,outproj1)
         tmpshp2<-try(subset(tmpshp1,PRESENCE < 3))
         tmpshp3<-try(subset(tmpshp2,ORIGIN < 3))
-        cats<-extract(rastg,tmpshp3,small=T) # extract categories
+        cats<-raster::extract(rastg,tmpshp3,small=T) # extract categories
         catsdf<-data.frame(value=unlist(cats),presabs=1) # create data frame with presabs
-        alldf<-merge(gridf,catsdf,all.x=TRUE) # merge with raster data frame 
+        alldf<-merge(gridf,catsdf,all.x=TRUE) # merge with raster data frame
         alldf$presabs<-replace(alldf$presabs,is.na(alldf$presabs),0) # insert 0s
         allr<-rasterFromXYZ(alldf[,c("x","y","presabs")]) # create final raster
-        filen<-paste(binomial,"_fullrange.tif",sep="") # filename 
+        filen<-paste(binomial,"_fullrange.tif",sep="") # filename
         fullpathfilen<-paste(outpath,"/",filen,sep="") # filename + path
         writeRaster(allr,file=fullpathfilen) # write raster
         lookup1<-data.frame(binomial,filename=filen) # data frame with file info
-        results<-rbind(lookup1,results)  # store data frame 
+        results<-rbind(lookup1,results)  # store data frame
         if(season=="TRUE"){
             # sesonal list (breeding,non-breeding,passage)
             seas.l<-list(c(1,2,5),c(1,3),c(4))
@@ -43,19 +43,19 @@ birdliferast<-function(speclist=NULL,inpath=NULL,outpath=NULL,rastg=NULL,season=
                 if(length(tmpshp.b$SEASONAL)==0){
                     next
                 } else {
-                    cats<-extract(rastg,tmpshp.b,small=T) # extract categories
+                    cats<-raster::extract(rastg,tmpshp.b,small=T) # extract categories
                     catsdf<-data.frame(value=unlist(cats),presabs=1) # create data frame with presabs
-                    alldf<-merge(gridf,catsdf,all.x=TRUE) # merge with raster data frame 
+                    alldf<-merge(gridf,catsdf,all.x=TRUE) # merge with raster data frame
                     alldf$presabs<-replace(alldf$presabs,is.na(alldf$presabs),0) # insert 0s
                     allr<-rasterFromXYZ(alldf[,c("x","y","presabs")]) # create final raster
-                    filen<-paste(binomial,"_",names(seas.l[z]),".tif",sep="") # filename 
+                    filen<-paste(binomial,"_",names(seas.l[z]),".tif",sep="") # filename
                     fullpathfilen<-paste(outpath,"/",filen,sep="") # filename + path
                     writeRaster(allr,file=fullpathfilen) # write raster
                     lookup1<-data.frame(binomial,filename=filen) # data frame with file info
-                    results<-rbind(lookup1,results)  # store data frame 
+                    results<-rbind(lookup1,results)  # store data frame
                 }
             }
-        }       
-    } 
+        }
+    }
     return(results)
-}     
+}
